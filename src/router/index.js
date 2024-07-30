5a542fad296a2a9c0850ebf013429c4b21d67155
Iryna <irynazankovska@gmail.com>
2024-07-30 14:43:32 -0400

    updates

--- src/router/index.js
+++ src/router/index.js
@@ -1,193 +1,209 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import store from "@/store"
+import { createRouter, createWebHistory } from "vue-router";
+import store from "@/store";
 import HomeView from "../views/HomeView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
-	 meta: {
-		requireAuth: false,
-	 },
+    meta: {
+      requireAuth: false,
+    },
     component: HomeView,
   },
   {
-	path: "/my-shop",
-	name: "my-shop",
-	meta: {
-		requireAuth: false,
-	 },
-	// route level code-splitting
-	// this generates a separate chunk (about.[hash].js) for this route
-	// which is lazy-loaded when the route is visited.
-	component: () =>
-	  import(/* webpackChunkName: "my-shop" */ "../views/MyShopView.vue"),
-},
+    path: "/my-shop",
+    name: "my-shop",
+    meta: {
+      requireAuth: false,
+    },
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "my-shop" */ "../views/MyShopView.vue"),
+  },
   {
     path: "/about",
     name: "about",
-	 meta: {
-		requireAuth: false,
-	 },
+    meta: {
+      requireAuth: false,
+    },
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
   {
-	path: "/contact-us",
-	name: "contact-us",
-	meta: {
-		requireAuth: false,
-	 },
-	component: () =>
-	  import(/* webpackChunkName: "contact-us" */ "../views/ContactUsView.vue"),
- },
- {
-	path: "/sign-up",
-	name: "sign-up",
-	meta: {
-		requireAuth: false,
-	 },
-	component: () =>
-	  import(/* webpackChunkName: "sign-up" */ "../components/Login/SignUpPage"),
- },
- {
-	path: "/login",
-	name: "login",
-	meta: {
-		requireAuth: false,
-	 },
-	component: () =>
-	  import(/* webpackChunkName: "login" */ "../components/Login/LoginPage"),
- },
- {
-	path: "/users",
-	name: "users",
-	meta: {
-		requireAuth: true,
-	 },
-	component: () =>
-	  import(/* webpackChunkName: "users" */ "../views/UsersView.vue"),
- },
- {
-	path: '/my-shop/individual-brushes',
-	name: "individual-brushes",
-	meta: {
-		requireAuth: false,
-	 },
-	component: () =>
-  import(/* webpackChunkName: "individual-brushes" */ "../components/My Shop/IndividualBrushes/BrushesList"),
-},
-{
-	path: '/my-shop/pro-create-set',
-	name: "pro-create-set",
-	meta: {
-		requireAuth: false,
-	 },
-	component: () =>
-  import(/* webpackChunkName: "pro-create-set" */ "../components/My Shop/ProCreateSet/ProCreateSetList"),
-},
-{
-	path: '/my-shop/travel-kit',
-	name: "travel-kit",
-	meta: {
-		requireAuth: false,
-	 },
-	component: () =>
-  import(/* webpackChunkName: "travel-kit" */ "../components/My Shop/TravelKit/TravelKitList"),
-},
- {
-	path: "/brush-editor/:brushId?",
-	name: "brush-editor",
-	meta: {
-		requireAuth: false,
-	 },
-	component: () =>
-	  import(/* webpackChunkName: "brush-editor" */ "@/components/My Shop/IndividualBrushes/BrushEditor.vue"),
-	// props: true,
- }, 
- {
-	path: "/proCreateSet-editor/:setId?",
-	name: "proCreateSet-editor",
-	meta: {
-		requireAuth: true,
-	 },
-	component: () =>
-	  import(/* webpackChunkName: "proCreateSet-editor" */ "@/components/My Shop/ProCreateSet/ProCreateSetEditor.vue"),
-	  props: true,
- 
-	}, 
- {
-	path: '/travelKit-editor/:kitId?',
-	name: "travelKit-editor",
-	meta: {
-		requireAuth: true,
-	 },
-	component: () =>
-  import(/* webpackChunkName: "travelKit-edito" */ "@/components/My Shop/TravelKit/TravelKitEditor"),
-  props: true,
-},
- {
-	path: "/cart/:productId?",
-	name: "cart",
-	meta: {
-		requireAuth: false,
-	 },
-	component: () =>
-	  import(/* webpackChunkName: "cart" */ "@/components/CartComponent.vue"),
- }, 
- {
-	path: "/brush-precautions",
-	name: "brush-precautions",
-	meta: {
-		requireAuth: false,
-	 },
-	component: () =>
-	  import(/* webpackChunkName: "brush-precautions" */ "@/components/BrushPrecautions.vue"),
- }, 
- {
-	path: "/return-policy",
-	name: "return-policy",
-	meta: {
-		requireAuth: false,
-	 },
-	component: () =>
-	  import(/* webpackChunkName: "return-policy" */ "@/components/ReturnPolicy.vue"),
- }, 
- {
-	path: "/my-account",
-	name: "my-account",
-	meta: {
-		requireAuth: true,
-	 },
-	component: () =>
-	  import(/* webpackChunkName: "my-account" */ "../views/MyAccount.vue"),
- }, 
- {
-	path: "/:pathMatch(.*)*",
-	name: "error",
-	component: () =>
-	  import(/* webpackChunkName: "error" */ "../components/ErrorPage.vue"),
- },
-
+    path: "/contact-us",
+    name: "contact-us",
+    meta: {
+      requireAuth: false,
+    },
+    component: () =>
+      import(/* webpackChunkName: "contact-us" */ "../views/ContactUsView.vue"),
+  },
+  {
+    path: "/sign-up",
+    name: "sign-up",
+    meta: {
+      requireAuth: false,
+    },
+    component: () =>
+      import(
+        /* webpackChunkName: "sign-up" */ "../components/Login/SignUpPage"
+      ),
+  },
+  {
+    path: "/login",
+    name: "login",
+    meta: {
+      requireAuth: false,
+    },
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../components/Login/LoginPage"),
+  },
+  {
+    path: "/users",
+    name: "users",
+    meta: {
+      requireAuth: true,
+    },
+    component: () =>
+      import(/* webpackChunkName: "users" */ "../views/UsersView.vue"),
+  },
+  {
+    path: "/my-shop/individual-brushes",
+    name: "individual-brushes",
+    meta: {
+      requireAuth: false,
+    },
+    component: () =>
+      import(
+        /* webpackChunkName: "individual-brushes" */ "../components/My Shop/IndividualBrushes/BrushesList"
+      ),
+  },
+  {
+    path: "/my-shop/pro-create-set",
+    name: "pro-create-set",
+    meta: {
+      requireAuth: false,
+    },
+    component: () =>
+      import(
+        /* webpackChunkName: "pro-create-set" */ "../components/My Shop/ProCreateSet/ProCreateSetList"
+      ),
+  },
+  {
+    path: "/my-shop/travel-kit",
+    name: "travel-kit",
+    meta: {
+      requireAuth: false,
+    },
+    component: () =>
+      import(
+        /* webpackChunkName: "travel-kit" */ "../components/My Shop/TravelKit/TravelKitList"
+      ),
+  },
+  {
+    path: "/brush-editor/:brushId?",
+    name: "brush-editor",
+    meta: {
+      requireAuth: false,
+    },
+    component: () =>
+      import(
+        /* webpackChunkName: "brush-editor" */ "@/components/My Shop/IndividualBrushes/BrushEditor.vue"
+      ),
+    // props: true,
+  },
+  {
+    path: "/proCreateSet-editor/:setId?",
+    name: "proCreateSet-editor",
+    meta: {
+      requireAuth: true,
+    },
+    component: () =>
+      import(
+        /* webpackChunkName: "proCreateSet-editor" */ "@/components/My Shop/ProCreateSet/ProCreateSetEditor.vue"
+      ),
+    props: true,
+  },
+  {
+    path: "/travelKit-editor/:kitId?",
+    name: "travelKit-editor",
+    meta: {
+      requireAuth: true,
+    },
+    component: () =>
+      import(
+        /* webpackChunkName: "travelKit-edito" */ "@/components/My Shop/TravelKit/TravelKitEditor"
+      ),
+    props: true,
+  },
+  {
+    path: "/cart/:productId?",
+    name: "cart",
+    meta: {
+      requireAuth: false,
+    },
+    component: () =>
+      import(/* webpackChunkName: "cart" */ "@/components/CartComponent.vue"),
+  },
+  {
+    path: "/brush-precautions",
+    name: "brush-precautions",
+    meta: {
+      requireAuth: false,
+    },
+    component: () =>
+      import(
+        /* webpackChunkName: "brush-precautions" */ "@/components/BrushPrecautions.vue"
+      ),
+  },
+  {
+    path: "/return-policy",
+    name: "return-policy",
+    meta: {
+      requireAuth: false,
+    },
+    component: () =>
+      import(
+        /* webpackChunkName: "return-policy" */ "@/components/ReturnPolicy.vue"
+      ),
+  },
+  {
+    path: "/my-account",
+    name: "my-account",
+    meta: {
+      requireAuth: true,
+    },
+    component: () =>
+      import(/* webpackChunkName: "my-account" */ "../views/MyAccount.vue"),
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "error",
+    component: () =>
+      import(/* webpackChunkName: "error" */ "../components/ErrorPage.vue"),
+  },
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
 router.beforeEach(async (to) => {
-	if (to.meta?.requireAuth) {
-		 let isAuthenticated = store.state.auth.user
+  if (to.meta?.requireAuth) {
+    let isAuthenticated = store.state.auth.user;
 
-		 if (!isAuthenticated) isAuthenticated = await store.dispatch('auth/loginWithCredential')
+    if (!isAuthenticated)
+      isAuthenticated = await store.dispatch("auth/loginWithCredential");
 
-		 if (!isAuthenticated)
-			  return {
-					name: 'login',
-			  }
-	}
-})
-
+    if (!isAuthenticated)
+      return {
+        name: "login",
+      };
+  }
+});
 
 export default router;
