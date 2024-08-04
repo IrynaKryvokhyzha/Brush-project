7bb057f34eb39f992585fe0149c67b2e14f06c2b
Iryna <irynazankovska@gmail.com>
2024-08-04 18:04:08 -0400

    update

--- src/components/ErrorPage.vue
+++ src/components/ErrorPage.vue
@@ -1,47 +1,46 @@
 <template>
-	<div class="error__container">
-		<h2 >Oops! Something went wrong!</h2>
-		<font-awesome-icon :icon="['fas', 'bug']" class="icon" />
-		<v-btn class="button" @click="onClick">Home</v-btn>
-	</div>
+  <div class="error__container">
+    <h2>Oops! Something went wrong!</h2>
+    <h2>Please try again later.</h2>
+    <font-awesome-icon :icon="['fas', 'bug']" class="icon" />
+    <v-btn class="button" @click="onClick">Home</v-btn>
+  </div>
 </template>
 
 <script>
-	export default {
-		name: 'ErrorPage',
-		methods: {
-			onClick() {
-				this.$router.push({
-					name: 'home'
-				})
-			}
-		},
-	}
+export default {
+  name: "ErrorPage",
+  methods: {
+    onClick() {
+      this.$router.push({
+        name: "home",
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .error {
+  // .error__container
 
-	// .error__container
-
-	&__container {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		padding-top: 50px;
-		gap: 30px;
-	}
+  &__container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding-top: 50px;
+    gap: 30px;
+  }
 }
-h2{
-	align-self: center;
+h2 {
+  align-self: center;
 }
 .icon {
-	color:  rgb(80, 136, 91);
-	font-size: 58px;
+  color: rgb(80, 136, 91);
+  font-size: 58px;
 }
 .button {
-	width: 200px;
-	align-self: center;
+  width: 200px;
+  align-self: center;
 }
-
 </style>
