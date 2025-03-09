5e42791d29e055792fd56201fccf69d410afeef1
Iryna <irynazankovska@gmail.com>
2025-03-09 14:14:23 -0400

    stopped site

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,68 +1,65 @@
 <template>
-		<main-master-page>
-			<div>
-				<video-component class="video"/>
-			</div>	
-			<div>
-				<my-shop-manager class="my-shop"/>
-			</div>
-			<div class="gentle-touch-component">
-				<gentle-touch/>
-			</div>
-			<div class="our-mission-component">
-				<our-mission/>
-			</div>
-			
-			
-		</main-master-page>
-		
+  <main-master-page>
+    <div>
+      <video-component class="video" />
+    </div>
+    <div>
+      <my-shop-manager class="my-shop" />
+    </div>
+    <div class="gentle-touch-component">
+      <gentle-touch />
+    </div>
+    <div class="our-mission-component">
+      <our-mission />
+    </div>
+  </main-master-page>
 </template>
 
 <script>
-
-import VideoComponent from '../components/VideoComponent.vue';
-import MainMasterPage from '../masterpages/MainMasterPage.vue';
-import MyShopManager from '../components/My Shop/MyShopManager.vue';
-import GentleTouch from '../components/GentleTouch.vue';
-import OurMission from '../components/OurMission.vue';
-
+import VideoComponent from "../components/VideoComponent.vue";
+import MainMasterPage from "../masterpages/MainMasterPage.vue";
+import MyShopManager from "../components/My Shop/MyShopManager.vue";
+import GentleTouch from "../components/GentleTouch.vue";
+import OurMission from "../components/OurMission.vue";
 
 export default {
   name: "HomeView",
-  components: { MainMasterPage, VideoComponent,  MyShopManager, GentleTouch, OurMission},
+  components: {
+    MainMasterPage,
+    VideoComponent,
+    MyShopManager,
+    GentleTouch,
+    OurMission,
+  },
 };
 </script>
 <style lang="scss" scoped>
-.wrapper{
-
+.wrapper {
 }
-.main-master{
-	display: flex;
-	flex-direction: column;
-
+.main-master {
+  display: flex;
+  flex-direction: column;
 }
-.video{
-
-	//margin-bottom: 50px;
-	width: 100%;
+.video {
+  //margin-bottom: 50px;
+  width: 100%;
 }
-.my-shop{
-	//position: relative;
-	margin-bottom: 10px;
-
+.my-shop {
+  //position: relative;
+  margin-bottom: 10px;
 }
-.gentle-touch-component{
-	//position: absolute;
-	width: 100%;
-	left: 0;
-	flex: 1 1 auto;
+.gentle-touch-component {
+  //position: absolute;
+  width: 100%;
+  left: 0;
+  flex: 1 1 auto;
 }
-.our-mission-component{
-	width: 100%;
-	background-color: rgb(116, 148, 122);
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
+.our-mission-component {
+  width: 100%;
+  background-color: rgb(116, 148, 122);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 </style>
